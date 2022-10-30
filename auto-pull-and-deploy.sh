83d0e50f3fd4c6cfb6b2d14db721cd83362df994
Michael <michimalek00@gmail.com>
2022-10-30 22:51:28 +0000

    Finished .sh script for deployment & updated docker-compose

--- auto-pull-and-deploy.sh
+++ auto-pull-and-deploy.sh
@@ -1,2 +1,7 @@
+# Pulling all updated images from google cloud registery & redis 
 docker pull gcr.io/de2022-362611/flask-celery-ml-celery
 docker pull gcr.io/de2022-362611/flask-celery-ml-app
+docker pull redis
+
+# Deploy images based on docker-compose including all env variables
+docker compose up -d
